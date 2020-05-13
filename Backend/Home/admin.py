a48a294f6366d00a9170e06c19c88c406d5d7fbd
IanMaguithi <maguithian13427@gmail.com>
2020-05-13 19:07:23 +0300

    modified the admin dashboard (admin.py) look by displaying more fields

--- Backend/Home/admin.py
+++ Backend/Home/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Destination
 
+
+class DisplayFields(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description', 'offer')
+
+
 # Register your models here.
-admin.site.register(Destination)
+admin.site.register(Destination, DisplayFields)
